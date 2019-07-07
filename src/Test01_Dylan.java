67398bd72b7d8017c9f24c9b50f9860e686011ec
노광훈 <undun01@gmail.com>
2019-07-07 17:14:18 +0900

    멤버 내용 수정

--- src/Test01_Dylan.java
+++ src/Test01_Dylan.java
@@ -3,7 +3,7 @@ public class Test01_Dylan {
 	public static void main(String[] args) {
 	
 		Member01 m01 
-		= new Member01("Dylan"); // <-요기!
+		= new Member01("왜 해결이 안되는걸까??"); // <-요기!
 		m01.print();
 	}
 
