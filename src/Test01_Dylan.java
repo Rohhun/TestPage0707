0fede648b741c69095d4ac8742527e9085f61fb9
midnight9040 <midnight9040@gmail.com>
2019-07-07 15:10:23 +0900

    //멤버 주소 올리기 Test

---
+++ src/Test01_Dylan.java
@@ -0,0 +1,10 @@
+/** 멤버 닉네임을 생성자 호출문장에 추가해주세요! **/
+public class Test01_Dylan {
+	public static void main(String[] args) {
+	
+		Member01 m01 
+		= new Member01("Dylan"); // <-요기!
+		m01.print();
+	}
+
+}
