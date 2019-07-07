f27ad1255ffea8cce0a3d5179bbbcc76614437a3
노광훈 <undun01@gmail.com>
2019-07-07 19:04:21 +0900

    커밋!

--- src/Test01_Dylan.java
+++ src/Test01_Dylan.java
@@ -3,7 +3,7 @@ public class Test01_Dylan {
 	public static void main(String[] args) {
 	
 		Member01 m01 
-		= new Member01("Dylan"); // <-요기!
+		= new Member01("커밋이 되긴할까... 뭐가문제일까"); // <-수정합니다!
 		m01.print();
 	}
 
